9611b06227f78347181f26e1cc17a93115ed70ff
pompos02 <yiannis.caravellas@gmail.com>
2025-08-17 14:53:02 +0300

    codebase cleanup

--- frontend/src/hooks/useColors.ts
+++
@@ -1,8 +0,0 @@
-import { rosePine, type RosePineColors } from '../colors';
-
-export const useColors = (): RosePineColors => {
-  return rosePine;
-};
-
-export { rosePine };
-export type { RosePineColors };
